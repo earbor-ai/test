1d6a676c113eb67861c6d865b2b8a767faea0445
manoj gokina <manojgokina@gmail.com>
2022-09-02 16:19:01 +0530

    Testing..

---
+++ src/components/Manoj.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Manoj = () => {
+  return (
+    <div>
+      <div>
+        This is a component 
+      </div>
+    </div>
+  )
+}
+
+export default Manoj
---
+++ src/components/Raghava.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Raghava = () => {
+  return (
+    <div>
+      <div>
+        This is also a component.
+      </div>
+    </div>
+  )
+}
+
+export default Raghava
